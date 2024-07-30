788cc2afd614750b911ee1b791b71461a3a65f05
Al Pogorelov <eczemplrez@gmail.com>
2024-07-30 21:17:43 +0600

    add get_date

--- src/widget.py
+++ src/widget.py
@@ -10,3 +10,13 @@ def mask_account_card(account_card_str: str) -> str:
         number = account_card_str[-16::]
         account_card_mask = account_card_str[:-16] + masks.get_mask_card_number(number)
     return account_card_mask
+
+
+def get_date(str_date_format: str) -> str:
+    '''Функция ворматирует дата вид ДД.ММ.ГГГГ'''
+    normal_format_date = ''
+    date_split = str_date_format.split('T')
+    date_split = date_split[0]
+    date_split = [date_split[-2:], date_split[-5:-3], date_split[:4]]
+    normal_format_date = '.'.join(date_split)
+    return normal_format_date
