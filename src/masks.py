921c3d754bdc72e62cf3de4575ff72effcaecd16
Al Pogorelov <eczemplrez@gmail.com>
2024-07-30 20:10:56 +0600

    start homework_9.2

---
+++ src/masks.py
@@ -0,0 +1,17 @@
+from typing import Iterable, Union
+
+
+def get_mask_card_number(card_number: Iterable[str]) -> str:
+    """Функция принмает номер карты, возвращает её маску типа "ХХХХ ХХ** **** ХХХХ" """
+    card_number_str = str(card_number)
+    card_number_split = [card_number_str[i : i + 6] for i in range(0, len(card_number_str), 6)]
+    card_number_split[1] = "******"
+    card_number_mask = "".join(card_number_split)
+    card_number_mask_ = [card_number_mask[i : i + 4] for i in range(0, len(card_number_mask), 4)]
+    return " ".join(card_number_mask_)
+
+
+def get_mask_account(number_account: Union[int, str]) -> str:
+    """Функция принмает номер счета и преобразует в маску с видом "**ХХХХ" """
+    number_account = str(number_account)
+    return f" **{number_account[-4:]}"
