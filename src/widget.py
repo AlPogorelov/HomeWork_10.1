6a6c32cb89f353fc130027788c2ee644d6edcad4
Al Pogorelov <eczemplrez@gmail.com>
2024-07-30 21:49:12 +0600

    final fixes

--- src/widget.py
+++ src/widget.py
@@ -1,8 +1,9 @@
 import masks
 
+
 def mask_account_card(account_card_str: str) -> str:
-    '''Функция принимает строку с номером карты или счета и  возвращает маску'''
-    account_card_mask = None
+    """Функция принимает строку с номером карты или счета и  возвращает маску"""
+    account_card_mask = ""
     if account_card_str[-20::].isdigit():
         number = account_card_str[-20::]
         account_card_mask = account_card_str[:-20] + masks.get_mask_account(number)
@@ -13,10 +14,11 @@ def mask_account_card(account_card_str: str) -> str:
 
 
 def get_date(str_date_format: str) -> str:
-    '''Функция ворматирует дата вид ДД.ММ.ГГГГ'''
-    normal_format_date = ''
-    date_split = str_date_format.split('T')
-    date_split = date_split[0]
-    date_split = [date_split[-2:], date_split[-5:-3], date_split[:4]]
-    normal_format_date = '.'.join(date_split)
+    """Функция ворматирует дата вид ДД.ММ.ГГГГ"""
+    normal_format_date = ""
+    date_split = list()
+    date_split = str_date_format.split("T")
+    date_split_ = date_split[0]
+    date_split = [date_split_[-2:], date_split_[-5:-3], date_split_[:4]]
+    normal_format_date = ".".join(date_split)
     return normal_format_date
